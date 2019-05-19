202f46b5653d4dff4cec68304cd8b97872426081
RafaelGoncalvesST16c <rafael.goncalves@edu.tbz.ch>
2019-05-19 20:25:16 +0200

    Massive Änderungen an den *.sh Dateien. Keine cp Befehle mehr.

--- LB2/Vagrantfile und Config/web.sh
+++ LB2/Vagrantfile und Config/web.sh
@@ -32,8 +32,6 @@ sudo find /var/www/html/wordpress -type d -exec chmod 2755 {} \;
 sudo chmod g+w /var/www/html/wordpress/wp-content
 sudo chmod -R g+w /var/www/html/wordpress/wp-content/themes
 sudo chmod -R g+w /var/www/html/wordpress/wp-content/plugins
-# wp-config.php kopieren
-sudo cp /var/www/html/Fileshare/wp-config.php /var/www/html/wordpress/wp-config.php
 # Apache neustarten
 sudo systemctl restart apache2
 # WP-CLI installieren und damit die WordPress Installation abschliessen
@@ -42,6 +40,20 @@ sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cl
 php wp-cli.phar --info
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+cd /var/www/html/wordpress
+sudo -u vagrant wp config set DB_NAME 'wordpress'
+sudo -u vagrant wp config set DB_USER 'wordpress'
+sudo -u vagrant wp config set DB_PASSWORD 'wordpress'
+sudo -u vagrant wp config set DB_HOST '10.0.0.11'
+sudo -u vagrant wp config set FS_METHOD 'direct'
+sudo -u vagrant wp config set AUTH_KEY '!ta+0sv-5fMp+IPQf-Hs1!|+Za5L|U42tn zG2Rk@5|>$7sVoX]pxaO(X#O2URa?'
+sudo -u vagrant wp config set SECURE_AUTH_KEY 'w`R>IF1uiy5D5g,Au(`~+.]u>y)d|c+&70_65kdj_.iGO@~XRUarx6G#o)[_ *h<'
+sudo -u vagrant wp config set LOGGED_IN_KEY 's6dL|+aq0$;5Y&0v5zEnYhn-!@{.nx&[-LDZ3:P.r-AxZ+YJBY!-oYSaB6Gv+F7?'
+sudo -u vagrant wp config set NONCE_KEY 'riJpyN}K@{&~fD90|,kJcV6.19yGQ!+ja!&r6R1)qE3_5-eXA-osx{R7h~7V!jP~?'
+sudo -u vagrant wp config set AUTH_SALT 'F<3?mr_bd!.^#+u n2T|t}_sqGP%;{$fCF@JkgfvWlkS$rZbZvn>-Cg*MNbd`E#R?'
+sudo -u vagrant wp config set SECURE_AUTH_SALT 'ub?zbFw]ej$4_=Rf&w`. jVk&`[Gu&D!=hE}#N&t`hE|3uB,#(NEYg$@))x7/5j{'
+sudo -u vagrant wp config set LOGGED_IN_SALT 'ChkK|55{no%U&rI+RfXzV6r-/i&l_(-{%xzFR@gipkwY@l;NY@pp0^vpT?-O?;Ec?'
+sudo -u vagrant wp config set NONCE_SALT 'e+Qs.4qBJc4*}$d~u-Y:AWXA+$DIUYo nNDU ,*8(gi[3Z] v>l.>R3HG0||(`1:'
 sudo -u vagrant -i -- wp core install --path=/var/www/html/wordpress --url=https://localhost:4343 --title="Meine WordPress Seite" --admin_name=wordpress --admin_email=wordpress@test.ch --admin_password=wordpress
 # Firewall anpassen und Logging aktivieren
 echo "y" | sudo ufw allow 80/tcp
@@ -51,16 +63,99 @@ echo "y" | sudo ufw allow 443/tcp
 echo "y" | sudo ufw enable
 # Reverse Proxy einrichten
 sudo a2enmod proxy proxy_html proxy_http
-sudo cp /var/www/html/Fileshare/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo rm /etc/apache2/sites-available/000-default.conf
+cat <<EOF | sudo tee -a /etc/apache2/sites-available/000-default.conf
+<IfModule mod_ssl.c>
+	<VirtualHost _default_:443>
+		ServerAdmin webmaster@localhost
+		ServerName 10.0.0.12
+		DocumentRoot /var/www/html/wordpress
+
+		ErrorLog ${APACHE_LOG_DIR}/error.log
+		CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+		SSLEngine on
+
+		SSLCertificateFile	/etc/ssl/certs/ssl-cert-snakeoil.pem
+		SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+
+		<FilesMatch "\.(cgi|shtml|phtml|php)$">
+		#SSLOptions +FakeBasicAuth +ExportCertData +StrictRequire
+				SSLOptions +StdEnvVars
+		</FilesMatch>
+		<Directory /usr/lib/cgi-bin>
+				SSLOptions +StdEnvVars
+		</Directory>
+
+		<Directory "/var/www/html/wordpress">
+			AuthType Basic
+			AuthName "Restricted Content"
+			AuthUserFile /etc/apache2/.htpasswd
+			Require valid-user
+		</Directory>
+
+		BrowserMatch "MSIE [2-6]" \
+			nokeepalive ssl-unclean-shutdown \
+			downgrade-1.0 force-response-1.0
+
+	</VirtualHost>
+</IfModule>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
 sudo service apache2 restart
+sudo sed '170i<Directory /var/www/html/wordpress>' /etc/apache2/apache2.conf
+sudo sed '171i  AllowOverride All' /etc/apache2/apache2.conf
+sudo sed '172i</Directory>' /etc/apache2/apache2.conf
 sudo cp /var/www/html/Fileshare/apache2.conf /etc/apache2/apache2.conf  
 # SSL aktivieren
 sudo a2ensite default-ssl.conf
 sudo a2dissite 000-default.conf
 sudo a2enmod ssl
-sudo cp /var/www/html/Fileshare/ports.conf /etc/apache2/ports.conf
+#sudo cp /var/www/html/Fileshare/ports.conf /etc/apache2/ports.conf
+sudo sed -i 's/Listen 80/#Listen 80/g' /etc/apache2/ports.conf
 sudo service apache2 restart
 # Authentisierung aktivieren
 printf 'vagrant\nvagrant' | sudo htpasswd -c /etc/apache2/.htpasswd guest
 sudo cp /var/www/html/Fileshare/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+sudo rm /etc/apache2/sites-available/default-ssl.conf
+cat <<EOF | sudo tee -a /etc/apache2/sites-available/default-ssl.conf
+<IfModule mod_ssl.c>
+	<VirtualHost _default_:443>
+		ServerAdmin webmaster@localhost
+		ServerName 10.0.0.12
+		DocumentRoot /var/www/html/wordpress
+
+		ErrorLog ${APACHE_LOG_DIR}/error.log
+		CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+		SSLEngine on
+
+		SSLCertificateFile	/etc/ssl/certs/ssl-cert-snakeoil.pem
+		SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+
+		<FilesMatch "\.(cgi|shtml|phtml|php)$">
+		#SSLOptions +FakeBasicAuth +ExportCertData +StrictRequire
+				SSLOptions +StdEnvVars
+		</FilesMatch>
+		<Directory /usr/lib/cgi-bin>
+				SSLOptions +StdEnvVars
+		</Directory>
+
+		<Directory "/var/www/html/wordpress">
+			AuthType Basic
+			AuthName "Restricted Content"
+			AuthUserFile /etc/apache2/.htpasswd
+			Require valid-user
+		</Directory>
+
+		BrowserMatch "MSIE [2-6]" \
+			nokeepalive ssl-unclean-shutdown \
+			downgrade-1.0 force-response-1.0
+
+	</VirtualHost>
+</IfModule>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
 sudo service apache2 restart
