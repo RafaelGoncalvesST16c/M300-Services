bad00f7b31dfa1a68a08f64cfb2ed61e461b83ef
RafaelGoncalvesST16c <rafael.goncalves@edu.tbz.ch>
2019-05-20 15:06:03 +0200

    CA Befehl angepasst

--- LB2/Vagrantfile und Config/ca.sh
+++ LB2/Vagrantfile und Config/ca.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 
 #Zertifikat erstellen
 sudo openssl genrsa 2048 > Test.ch.key
-sudo openssl req -new -key ./Test.ch.key -subj "/C=ZH/ST=Zurich/L=Zurich/O=Test GmbH/OU=Test Abteilung/CN=Test.ch" > Test.ch.csr
+sudo openssl req -new -x509 -extensions v3_ca -key ./Test.ch.key -subj "/C=ZH/ST=Zurich/L=Zurich/O=Test GmbH/OU=Test Abteilung/CN=Test.ch" > Test.ch.csr
 sudo openssl x509 -in Test.ch.csr -out Test.ch.crt -req -signkey Test.ch.key -days 365
 #Zertifikat kopieren in Fileshare
 sudo cp Test.ch.crt /var/www/html/Fileshare/Test.ch.crt
